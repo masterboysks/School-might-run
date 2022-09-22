ab01ac6746e265e9f4ce5d309d46f2a7f04968e3
masterboysks1 <saurav@spellinnovation.com>
2022-09-22 17:04:10 +0545

    Changes to routes how they are maintained

--- sms-company/src/routes/index.jsx
+++ sms-company/src/routes/index.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import TeacherRoutes from "./teacher";
 import AdminRoutes from "./admin";
-import ParentRoutes from "./parent";
-import StudentRoutes from "./student";
+import UserRoutes from "./user/UserRoutes";
 
 export default function MainRoutes() {
   return (
@@ -12,8 +11,12 @@ export default function MainRoutes() {
       <Route path="/">
         <Route index element={<Login />} />
         <Route path="admin/*" element={<AdminRoutes />} />
-        <Route path="student/*" element={<StudentRoutes />} />
-        <Route path="parent/*" element={<ParentRoutes />} />
+        <Route path="student" element={<Outlet />}>
+          <Route path="dashboard/*" element={<UserRoutes />} />
+        </Route>
+        <Route path="parent" element={<Outlet />}>
+          <Route path="dashboard/*" element={<UserRoutes />} />
+        </Route>
         <Route path="teacher/*" element={<TeacherRoutes />} />
         <Route path="*" element={<>404 not found</>} />
       </Route>
