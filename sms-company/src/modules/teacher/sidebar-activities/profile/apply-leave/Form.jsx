8a51899f737bba89549a52233a711e6d9c8a3b24
masterboysks1 <saurav@spellinnovation.com>
2022-09-22 12:14:03 +0545

    Teacher profile and apply leave complete

--- sms-company/src/modules/teacher/sidebar-activities/profile/apply-leave/Form.jsx
+++ sms-company/src/modules/teacher/sidebar-activities/profile/apply-leave/Form.jsx
@@ -1,40 +1,67 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 import { Input, Textarea } from "../../../../../components/common/fields";
 
 export default function Form() {
   const {
     register,
     formState: { errors },
+    handleSubmit,
   } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <div className="my-6">
-      <div className="">
-        <Input
-          register={register}
-          name="start-date"
-          type="date"
-          label="Start date*"
-          required={true}
-          errors={errors}
-        />
-      </div>
-      <div
-        className="
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="my-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid grid-cols-1 gap-4">
+        <div className="">
+          <Input
+            register={register}
+            name="start-date"
+            type="date"
+            label="Start date*"
+            required={true}
+            errors={errors}
+          />
+        </div>
+        <div
+          className="
         "
-      >
-        <Input
-          register={register}
-          name="end-date"
-          type="date"
-          label="End date*"
-          required={true}
-          errors={errors}
-        />
+        >
+          <Input
+            register={register}
+            name="end-date"
+            type="date"
+            label="End date*"
+            required={true}
+            errors={errors}
+          />
+        </div>
       </div>
-      <div className="">
-        <Textarea register={register} name="reason" label="Reason " />
+      <div className="my-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid grid-cols-1 gap-4">
+        <div className=" col-span-2">
+          <Textarea register={register} name="reason" label="Reason " />
+        </div>
       </div>
-    </div>
+      <div className="sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid grid-cols-1 gap-4">
+        <div className="md:flex-row w-fit col-span-full lg:col-span-2 flex flex-col my-6 ml-auto">
+          <div className=" w-fit">
+            <Link
+              to="/admin/dashboard/admin/data-setup/faculty"
+              className="bg-primary-grey-50 text-primary-grey-700 hover: focus:outline-none focus:ring- focus:ring-offset-2 sm:w-auto inline-flex items-center justify-center px-4 py-3 mr-3 text-sm font-medium border border-transparent rounded-md shadow-sm"
+            >
+              Cancel
+            </Link>
+            <button
+              type="submit"
+              className="bg-primary-btn hover: focus:outline-none focus:ring- focus:ring-offset-2 sm:w-auto inline-flex items-center justify-center px-4 py-3 text-sm font-medium text-white border border-transparent rounded-md shadow-sm"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
   );
 }
