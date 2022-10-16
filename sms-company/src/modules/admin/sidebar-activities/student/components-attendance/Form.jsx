1fd0035ffa10a0df723c6c651f2d472652a22dbd
masterboysks1 <saurav@spellinnovation.com>
2022-10-16 16:04:03 +0545

    student attendence and information main page search form complete design changed

--- sms-company/src/modules/admin/sidebar-activities/student/components-attendance/Form.jsx
+++ sms-company/src/modules/admin/sidebar-activities/student/components-attendance/Form.jsx
@@ -1,44 +1,55 @@
 import React from "react";
 import Search from "@mui/icons-material/SearchOutlined";
+import { useForm } from "react-hook-form";
+import { Select } from "../../../../../components/common/fields";
+const arrayLevel = [
+  { name: "anish", id: 7 },
+  { name: "saurav", id: 4 },
+  { name: "asmita", id: 3 },
+  { name: "prahlad", id: 2 },
+];
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm();
   return (
     <div className="ring-black ring-opacity-5 ring-1 form-solid mb-14 w-full p-4 my-6 rounded-md shadow">
       <form className="sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid grid-cols-1 gap-4">
         <div className="">
-          <label className="my-6 text-sm" htmlFor="Status">
-            Level
-          </label>
-          <br />
-          <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
-            <option value="Test">Select</option>
-          </select>
+          <Select
+            value={arrayLevel}
+            register={register}
+            label="Level"
+            name="level"
+          />
         </div>
         <div className="">
-          <label className="my-6 text-sm" htmlFor="Status">
-            Class/Semester
-          </label>
-          <br />
-          <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
-            <option value="Test">Select</option>
-          </select>
+          <Select
+            label="CLass/Semester"
+            register={register}
+            name="class"
+            value={arrayLevel}
+          />
         </div>
         <div className="">
-          <label className="my-6 text-sm" htmlFor="Status">
-            Faculty
-          </label>
-          <br />
-          <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
-            <option value="Test">Select</option>
-          </select>
+          <Select
+            label="Faculty"
+            register={register}
+            name="faculty"
+            value={arrayLevel}
+          />
         </div>
         <div className="">
-          <label className="my-6 text-sm" htmlFor="Status">
-            Section
-          </label>
-          <br />
-          <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
-            <option value="Test">Select</option>
-          </select>
+          <Select
+            label="Section"
+            register={register}
+            name="section"
+            value={arrayLevel}
+          />
         </div>
         <div className="col-span-full lg:col-span-2 xl:col-span-full">
           <div className="bg-primary-btn h-fit w-fit hover: focus:outline-none focus:ring- focus:ring-offset-2 flex items-center justify-center p-2 ml-auto text-sm font-medium text-white border border-transparent rounded-md shadow-sm cursor-pointer">
--- sms-company/src/modules/admin/sidebar-activities/student/student-information/Form.jsx
+++ sms-company/src/modules/admin/sidebar-activities/student/student-information/Form.jsx
@@ -1,5 +1,7 @@
 import Search from "@mui/icons-material/SearchOutlined";
+import { useForm } from "react-hook-form";
 import Breadnav from "../../../../../components/common/Breadnav";
+import { Input, Select } from "../../../../../components/common/fields";
 
 const pages = [
   { name: "Student", href: "#", current: false },
@@ -9,78 +11,80 @@ const pages = [
     current: true,
   },
 ];
+const arrayLevel = [
+  { name: "anish", id: 7 },
+  { name: "saurav", id: 4 },
+  { name: "asmita", id: 3 },
+  { name: "prahlad", id: 2 },
+];
 
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm();
   return (
     <>
       <Breadnav pages={pages} />
       <form className="sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ring-1 ring-black ring-opacity-5 form-solid grid grid-cols-1 gap-4 p-4 my-6 rounded-md shadow">
         <div className="">
-          <label className="my-6 text-sm" htmlFor="Student Id">
-            Student Id
-          </label>
-          <br />
-          <input
-            className="w-full p- rounded  focus:ring-primary-btn mt-[6px]    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm"
-            type="text"
-            placeholder="Id"
+          <Input
+            label="Student ID"
+            placeholder="9955112"
+            register={register}
+            name="student_id"
           />
         </div>
         <div className="">
-          <label className="my-6 text-sm" htmlFor="Student Name">
-            Student Name
-          </label>
-          <br />
-          <input
-            className="w-full p- rounded  focus:ring-primary-btn mt-[6px]    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm"
-            type="text"
-            placeholder="Student Name"
+          <Input
+            label="Student Name"
+            placeholder="Susank"
+            register={register}
+            name="student_name"
           />
         </div>
         <div className="">
-          <label className="my-6 text-sm" htmlFor="Class / Semester">
-            Class / Semester
-          </label>
-          <br />
-          <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
-            <option value="Test">Select</option>
-          </select>
+          <Select
+            value={arrayLevel}
+            register={register}
+            name="class_id"
+            label="Class/ Semester"
+          />
         </div>
         <div className="">
-          <label className="my-6 text-sm" htmlFor="Faculty">
-            Faculty
-          </label>
-          <br />
-          <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
-            <option value="Test">Select</option>
-          </select>
+          <Select
+            value={arrayLevel}
+            register={register}
+            name="faculty"
+            label="Faculty"
+          />
         </div>
         <div className="">
-          <label className="my-6 text-sm" htmlFor="Section">
-            Section
-          </label>
-          <br />
-          <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
-            <option value="Test">Select</option>
-          </select>
+          <Select
+            value={arrayLevel}
+            register={register}
+            name="section"
+            label="Section"
+          />
         </div>
         <div className="">
-          <label className="my-6 text-sm" htmlFor="Status">
-            Status
-          </label>
-          <br />
-          <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn    border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
-            <option value="Test">Select</option>
-          </select>
+          <Select
+            value={arrayLevel}
+            register={register}
+            name="status"
+            label="Status"
+          />
         </div>
         <div className="">
-          <label className="my-6 text-sm" htmlFor="Order By">
-            Order By
-          </label>
-          <br />
-          <select className="w-full p-2  cursor-pointer rounded mt-[6px]  focus:ring-primary-btn     border-primary-field shadow-md placeholder:text-primary-grey-400   text-primary-grey-700 text-sm">
-            <option value="Test">Select</option>
-          </select>
+          <Select
+            value={arrayLevel}
+            register={register}
+            name="order_by"
+            label="Order By"
+          />
         </div>
         <div className="h-fit w-fit bg-primary-btn sm:box-content lg:col-span-2 xl:col-span-1 box-border px-4 py-3 mt-auto ml-auto text-white rounded cursor-pointer">
           {/* <span className="sm:hidden text-primary-grey-100 text-sm">Search</span> */}
