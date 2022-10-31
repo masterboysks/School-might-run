b2a077989dd5b253ca4b64d046e56ce5fa91be75
masterboysks1 <saurav@spellinnovation.com>
2022-10-31 15:05:28 +0545

    bs_ad deleted new one will replace

--- sms-company/src/components/common/fields/bs_ad.jsx
+++ sms-company/src/components/common/fields/bs_ad.jsx
@@ -1,80 +1,5 @@
 import React from "react";
 import { InputNumber, Select } from ".";
-const arrayMonths = [
-  {
-    bs: "Baishakh",
-    name: "January",
-    id: 1,
-  },
-  {
-    bs: "Jestha",
-    name: "February",
-    id: 2,
-  },
-  {
-    bs: "Aashadha",
-    name: "March",
-    id: 3,
-  },
-  { bs: "Shrawan", name: "April", id: 4 },
-  {
-    bs: "Bhadra",
-    name: "May",
-    id: 5,
-  },
-  {
-    bs: "Ashwin ",
-    name: "June",
-    id: 6,
-  },
-  {
-    bs: "Kartik ",
-    name: "July",
-    id: 7,
-  },
-  {
-    bs: "Mangsir ",
-    name: "August",
-    id: 8,
-  },
-  {
-    bs: "Paush ",
-    name: "September",
-    id: 9,
-  },
-  {
-    bs: "Magh ",
-    name: "October",
-    id: 10,
-  },
-  {
-    bs: "Falgun ",
-    name: "November",
-    id: 11,
-  },
-  {
-    bs: "Chaitra ",
-    name: "December",
-    id: 12,
-  },
-];
-
-export const bs_ad_month = (props) => {
-  const format = localStorage.getItem("hdjkjkfg");
-  return (
-    <Select
-      value={
-        format === "8cef65fa-4dd7-11ed-bdc3-0242ac120002"
-          ? arrayMonths.map((c) => {
-              return { id: c.id, name: c.bs };
-            })
-          : arrayMonths
-      }
-      {...props}
-      name={`${props.name}[month]`}
-    ></Select>
-  );
-};
 
 export const bs_ad_year = (props) => {
   const format = localStorage.getItem("hdjkjkfg");
@@ -89,18 +14,6 @@ export const bs_ad_year = (props) => {
       label={`${props.label}(${
         format === "8cef65fa-4dd7-11ed-bdc3-0242ac120002" ? "BS" : "AD"
       })`}
-    ></InputNumber>
-  );
-};
-export const bs_ad_day = (props) => {
-  return (
-    <InputNumber
-      placeholder="16"
-      {...props}
-      name={`${props.name}[day]`}
-      min="1"
-      max="32"
-      step="1"
     ></InputNumber>
   );
 };
