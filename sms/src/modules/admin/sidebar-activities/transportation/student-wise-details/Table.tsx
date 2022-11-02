d6946659410193422d0138c02a1c0857a242c649
masterboysks1 <saurav@spellinnovation.com>
2022-11-02 17:17:37 +0545

    student wise details
    routes ready for development basic structure defined

---
+++ sms/src/modules/admin/sidebar-activities/transportation/student-wise-details/Table.tsx
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import RenderTable from "./RenderTable";
+const data = [
+  {
+    id: "45sad5",
+    name: "445",
+    route_name: "ndnbjk",
+    station: "jhdfsjgdfs",
+    trip_type: "jkdhfjhksf",
+    fee: "jkfhjkh",
+    status: 0,
+  },
+  {
+    id: "4sa55",
+    name: "445",
+    route_name: "ndnbjk",
+    station: "jhdfsjgdfs",
+    trip_type: "jkdhfjhksf",
+    fee: "jkfhjkh",
+    status: 0,
+  },
+  {
+    id: "453asfa25",
+    name: "445",
+    route_name: "ndnbjk",
+    station: "jhdfsjgdfs",
+    trip_type: "jkdhfjhksf",
+    fee: "jkfhjkh",
+    status: 1,
+  },
+  {
+    id: "4fa3w55",
+    name: "445",
+    route_name: "ndnbjk",
+    station: "jhdfsjgdfs",
+    trip_type: "jkdhfjhksf",
+    fee: "jkfhjkh",
+    status: 0,
+  },
+  {
+    id: "45fas5",
+    name: "445",
+    route_name: "ndnbjk",
+    station: "jhdfsjgdfs",
+    trip_type: "jkdhfjhksf",
+    fee: "jkfhjkh",
+    status: 1,
+  },
+];
+export default function Table() {
+  return (
+    <div className="mt-11 mb-6">
+      {data.length === 0 ? (
+        <div className="flex justify-center text-2xl my-9">
+          Please search to view Table
+        </div>
+      ) : (
+        <>
+          <div className="my-6">
+            <div className=" ring-1 overflow-x-auto overflow-y-auto ring-black ring-opacity-5 min-w-full rounded-lg shadow">
+              <div className="inline-block w-full align-middle">
+                <div className=" w-full rounded-lg">
+                  <table className="min-w-full divide-y divide-gray-300 table-auto">
+                    <thead className="bg-gray-50">
+                      <tr>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-medium text-primary-grey-700    "
+                        >
+                          Student ID
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-medium text-primary-grey-700   "
+                        >
+                          Student Name
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-medium text-primary-grey-700   "
+                        >
+                          Route Name
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-medium text-primary-grey-700   "
+                        >
+                          Station
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-medium text-primary-grey-700   "
+                        >
+                          Trip type
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-medium text-primary-grey-700   "
+                        >
+                          Transp. fee(Rs)
+                        </th>
+
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-medium text-primary-grey-700   "
+                        >
+                          Status
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="bg-white divide-y divide-gray-200">
+                      <RenderTable currentItems={data} />
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <Pagination setPage={setPage} pagination={pagination} /> */}
+        </>
+      )}
+    </div>
+  );
+}
--- sms/src/modules/admin/sidebar-activities/transportation/student-wise-details/page.tsx
+++ sms/src/modules/admin/sidebar-activities/transportation/student-wise-details/page.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import Breadnav from "../../../../../components/common/Breadnav";
+import Table from "./Table";
 
+const pages = [
+  { name: "Transportation", href: "#", current: false },
+  {
+    name: "Student wise details",
+    href: "/admin/dashboard/transport/student-wise-details/",
+    current: true,
+  },
+];
 export default function StudentWiseDetails() {
-  return <div>StudentWiseDetails</div>;
+  return (
+    <>
+      <Breadnav pages={pages} />
+      Search left its custonnm
+      <Table></Table>
+    </>
+  );
 }
